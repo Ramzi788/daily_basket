184c084e24b2a218d7162b824a68f7126038b85b
Ramzi788 <Rzeineddine200382@gmail.com>
2023-06-21 18:03:11 +0300

    changes

---
+++ lib/Constants/themes.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primaryColor: Colors.green,
+    // primaryColorDark: const Color.fromARGB(255, 96, 165, 250),
+    // primaryColorLight: const Color.fromARGB(255, 27, 31, 77),
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: const Color(0xFFF5F5F5),
+    textTheme: const TextTheme(
+      headline1: TextStyle(
+          fontSize: 55, fontWeight: FontWeight.bold, fontFamily: 'Raleway'),
+      headline2: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+      headline3: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+      headline4: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+      headline6: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+      ),
+      headline5: TextStyle(fontSize: 25, fontWeight: FontWeight.normal),
+      bodyText1: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+    ),
+  );
+}
--- lib/Screens/GetStartedScreen.dart
+++ lib/Screens/GetStartedScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class GetStartedScreen extends StatefulWidget {
   const GetStartedScreen({super.key});
@@ -10,10 +11,48 @@ class GetStartedScreen extends StatefulWidget {
 class _GetStartedScreenState extends State<GetStartedScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
-    );
+    return Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/basket.jpg'))),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Lottie.network(
+              //     'https://assets7.lottiefiles.com/private_files/lf30_vb7v5ca0.json'),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  "We deliver \ngrocery to your\ndoorstep",
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.4),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
